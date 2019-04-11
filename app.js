454811df6784ad901c5ed3ebc5ceef9bd1b82bbe
liornoy <40122521+liornoy@users.noreply.github.com>
2019-04-11 14:43:34 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
 el: '#vue-app',
 data:{
     message: '',
-    tasks: ['Learn C++','Build a 2D game', 'Upgrade the website']
+    tasks: ['Learn C++','Build a 2D game', 'Upgrade the website'],
+    completed: '',
+    count: 0
 },
 methods: {
     enterTask () {
@@ -13,6 +15,17 @@ methods: {
     },
     deleteTask(index){
     this.tasks.splice(index, 1);
+    console.log("hi1");
+  },
+    completes(index){
+      this.count ++;
+      console.log("hi2");
+      this.completed+=this.tasks[index]+", ";
+      this.tasks.splice(index, 1);
     }
+},
+computed: {
+  complet(){return this.completed},
+  long(){return this.count}
 }
 });
